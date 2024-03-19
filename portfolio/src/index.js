c937ceb018803a1f8b549b2797f71650c06a571c
SempreGM <bernardomaia57@gmail.com>
2024-03-19 15:34:07 -0300

    Routes

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Home from './Home/home';
@@ -9,4 +10,5 @@ root.render(
     <Home/>
   </React.StrictMode>
 );
+
 reportWebVitals();
